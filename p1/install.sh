368f71f00c98b3fd5d30e891614b16b6528ad6f4
patrick <zouave@gmail.com>
2024-05-19 09:24:07 +0000

    avancement p3

--- p1/install.sh
+++ p1/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 apt-get update
-apt-get install vagrant -y
-apt-get install virtualbox -y
+apt-get install vagrant virtualbox vim git make -y
 apt-get upgrade
+vagrant plugin install vagrant-vbguest
---
+++ p3/config.sh
@@ -0,0 +1,7 @@
+sudo k3d cluster create mycluster
+sudo kubectl create namespace dev
+sudo kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+wget https://github.com/argoproj/argo-cd/releases/download/v2.11.0/argocd-linux-amd64 -O argocd
+sudo chmod +x argocd
+sudo mv argocd /usr/local/bin/
