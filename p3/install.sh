2a5ed52194f838af4c208a96e8062c279f438a59
zarakel <jerome.buan.06@gmail.com>
2024-05-22 07:06:33 +0000

    useless

--- p3/install.sh
+++ p3/install.sh
@@ -4,3 +4,4 @@ sudo sh get-docker.sh # continuité ligne précédente
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" # install kubectl avec le ligne suivante
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl # continuité ligne précédente
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash # installer helm
+sudo bash config.sh
