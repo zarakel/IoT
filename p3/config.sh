9333deb6c5c0290ca3f3170d2e267893fa40cff4
zarakel <jerome.buan.06@gmail.com>
2024-05-22 13:51:49 +0000

    os stupide 2

--- p3/config.sh
+++ p3/config.sh
@@ -8,7 +8,7 @@ sudo mv argocd /usr/local/bin/
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 argocd admin initial-password -n argocd | awk 'NR==1' | cat > pass
 kubectl get nodes -o wide | awk -v OFS='\t\t' '{print }' | awk 'NR==2' | cat > ip_cluster 
-argocd login `cat ip_address`:`cat port` --username admin --pass `cat pass`
+argocd login `cat ip_address`:`cat port` --username admin --password `cat pass`
 argocd app create playground --repo https://github.com/zarakel/ArgoCD-k3d-pipe-.git --path playground --dest-server https://kubernetes.default.svc --dest-namespace dev
 argocd app set playground --sync-policy automated
 #kubectl patch svc playground -n dev -p '{"spec": {"type": "LoadBalancer"}}' Je la commente car pas sur de m'en servir puisque le lb est déja fait
