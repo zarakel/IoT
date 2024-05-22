0e0ef9bb32bbb67b2baba11508f71b91af4561b4
zarakel <jerome.buan.06@gmail.com>
2024-05-22 13:56:15 +0000

    sudo forever

--- p3/config.sh
+++ p3/config.sh
@@ -1,13 +1,13 @@
 sudo k3d cluster create mycluster -a 1
 sudo kubectl create namespace dev
 sudo kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-wget https://github.com/argoproj/argo-cd/releases/download/v2.11.0/argocd-linux-amd64 -O argocd
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sudo wget https://github.com/argoproj/argo-cd/releases/download/v2.11.0/argocd-linux-amd64 -O argocd
 sudo chmod +x argocd
 sudo mv argocd /usr/local/bin/
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+sudo kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 argocd admin initial-password -n argocd | awk 'NR==1' | cat > pass
-kubectl get nodes -o wide | awk -v OFS='\t\t' '{print }' | awk 'NR==2' | cat > ip_cluster 
+sudo kubectl get nodes -o wide | awk -v OFS='\t\t' '{print }' | awk 'NR==2' | cat > ip_cluster 
 argocd login `cat ip_address`:`cat port` --username admin --password `cat pass`
 argocd app create playground --repo https://github.com/zarakel/ArgoCD-k3d-pipe-.git --path playground --dest-server https://kubernetes.default.svc --dest-namespace dev
 argocd app set playground --sync-policy automated
--- p3/install.sh
+++ p3/install.sh
@@ -1,8 +1,8 @@
 sudo apt install wget curl -y 
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash # install k3D
-curl -fsSL https://get.docker.com -o get-docker.sh # install docker engine
+sudo wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash # install k3D
+sudo curl -fsSL https://get.docker.com -o get-docker.sh # install docker engine
 sudo sh get-docker.sh # continuité ligne précédente
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" # install kubectl avec le ligne suivante
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" # install kubectl avec le ligne suivante
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl # continuité ligne précédente
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash # installer helm
+sudo curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash # installer helm
 sudo bash config.sh
